903ac48809441ceb8b7ccc2228db239cd49b41ad
nocturne.pe <nocturne.pe@gmail.com>
2011-05-23 12:51:44 +0000

    Mudando para Flash

--- cliente/src/NHEClient/libs/sim-2.1/src/sim.mk
+++
@@ -1,4 +0,0 @@
-OBJECTS = tytree.o simul.o colist.o entity.o \
-        simevent.o error.o generate.o histo.o analysis.o \
-        queue.o resource.o reports.o coutanal.o schedule.o \
-        screen.o handlers.o control.o clock.o query.o
